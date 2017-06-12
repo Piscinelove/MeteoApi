1c12c28ccf9358f845edb41198b6deb4e72813d5
Rafael Peixoto <peixotterafael@gmail.com>
2017-06-12 10:04:07 +0200

    Required

--- MeteoMVC/DTO/Weather.cs
+++ MeteoMVC/DTO/Weather.cs
@@ -12,20 +12,27 @@ namespace DTO
 
         public int Id { get; set; }
 
+        [Required]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
 
+        [Required]
         public double TemperatureMin { get; set; }
 
+        [Required]
         public double TemperatureMax { get; set; }
 
+        [Required]
         public double Humidity { get; set; }
 
+        [Required]
         public double Precipitation { get; set; }
 
+        [Required]
         public City City { get; set; }
 
+        [Required]
         public State State { get; set; }
     }
 }
